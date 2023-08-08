0cbe227b64ce668d1281a5e912f5a2651ea83c23
Peleg <pelegshefi@hotmail.com>
2023-08-08 17:49:30 +0300

    GYMNASIUM ENV

--- src/ab/python_agent/agent/main.py
+++ src/ab/python_agent/agent/main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from os.path import dirname, abspath, join
 import sys
+import re
 
 # Find code directory relative to our directory
 THIS_DIR = dirname(__file__)
@@ -48,23 +49,14 @@ class AngryBirdGame(Env):
 
     def step(self, action):
         # Action key - [dx,dy]
-        action_map = {
-            0: [-10, 10],
-            1: [-10, 20],
-            2: [-10, 30],
-            3: [-20, 40],
-            4: [-30, 50],
-            5: [-40, 20],
-
-        }
-        # SEND ACTION TO SERVER
-        makeshot = self.ar.c_shoot(196, 326, action_map[action][0], action_map[action][1], 0,
+        print(f'using action: {action}')
+        # SEND ACTION TO SERVER (196,326)
+        makeshot = self.ar.c_shoot(191, 344, ACTION_MAP[action][0], ACTION_MAP[action][1], 0,
                                    0)  # shot in cartesian random values
-
         # Get the next observation
         new_observation = self.get_observation()
         # Check if game is done
-        done, lvl = self.get_done()
+        done, is_win = self.get_done()
         if not done:
             img_dir = self.ar.do_screen_shot()
             image = cv2.imread(img_dir)
@@ -75,13 +67,18 @@ class AngryBirdGame(Env):
             (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 170, 255, cv2.THRESH_BINARY)
             res = pytesseract.image_to_string(blackAndWhiteImage, config="--psm 10")
             # reward = int(res) if self._check_OCR(res,blackAndWhiteImage) else 0
-            reward = int(res)
+            if re.match("^[0-9 ]+$", res):
+                reward = int(res)
+            else:
+                reward = 0
         else:
             score = self.ar.get_my_score()
             reward = score[self.current_level - 1]
-        # Info dict - not relevant
-        info = {'lvl': lvl}
 
+        # Info dict - not relevant
+        info = {'is_win': is_win["is_win"]}
+        if info["is_win"]:
+            self.current_level = self.current_level + 1
         return new_observation, reward, done, False, info
 
     def render(self):
@@ -117,15 +114,14 @@ class AngryBirdGame(Env):
         state = self.ar.get_state()[0]
         if state == STATE_WON:
             self.solved[self.current_level - 1] = 1
-            self.current_level = self.current_level + 1
-            return True, {'current_lvl': self.current_level - 1}
+            return True, {'is_win': True}
         elif state == STATE_LOST:
-            return True, {'current_lvl': self.current_level}
+            return True, {'is_win': False}
         elif state == STATE_PLAYING:
-            return False, {'current_lvl': self.current_level}
+            return False, {'is_win': False}
         else:
             print('not good err')
-            return True, {}
+            return True, {'is_win': False}
 
     def _get_next_level(self):
         level = 0
@@ -190,31 +186,47 @@ class TrainAndLoggingCallback(BaseCallback):
 
 def train(env):
     for episode in range(5):
-        obs = env.reset()
+        # obs = env.reset()
         done = False
         total_reward = 0
         lvl = 1
         while not done:
             obs, reward, done, trunct, info = env.step(env.action_space.sample())
-            print(f'playing level: {info["lvl"]} got reward: {reward} / {total_reward}')
-            total_reward += reward
-            lvl = info["lvl"]
-        print(f'Total reward for episode {episode} is {total_reward} in level: {lvl}')
+
+            total_reward = total_reward + (reward - total_reward)
+        print(f'Total reward for episode {episode} is {total_reward}')
+        print('______________________________')
+
+
+def test_model(env):
+    model_path = f'{MODELS_DIR}/best_model_5000.zip'
+    model = DQN.load(model_path, env)
+    for episode in range(5):
+        obs = env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            action, _ = model.predict(obs)
+            obs, reward, done, trunct, info = env.step(int(action))
+            time.sleep(0.01)
+            total_reward = total_reward + (reward - total_reward)
+        print(f'Total reward for episode {episode} is {total_reward}')
         print('______________________________')
 
 
 print('started to run')
 
 env = AngryBirdGame()
-train(env)
-# env_checker.check_env(env)
+env_checker.check_env(env)
 callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
 
-# TRAIN TODO: implement
 # create DQN model policy, the enviorment, where to save logs, verbose logs, how big is the buffer depends on RAM,
 # start training after 1000 steps
-# model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=1200000, learning_starts=1000)
-# print(model)
-#
-# # kickoff training
-# model.learn(total_timesteps=100000, callback=callback)
+model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=12000, learning_starts=1000)
+print(model)
+
+# TRAIN
+model.learn(total_timesteps=100000, callback=callback)
+
+# TESTING
+# test_model(env)
